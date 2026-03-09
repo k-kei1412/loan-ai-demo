4a75b105933599076a858fea4696c33f818c3a28
k-kei1412 <keisuke3375@gmail.com>
2026-03-09 21:06:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -203,6 +203,9 @@ if submit:
             imp_df['adj'] = imp_df['raw']
             imp_df.loc[imp_df['項目'] == 'TermInMonths', 'adj'] *= 0.23
             imp_df.loc[imp_df['項目'] == 'GrossApproval', 'adj'] *= 1.7
+            imp_df.loc[imp_df['項目'] == 'SBAGuaranteedApproval', 'adj'] *= 0.8
+            imp_df.loc[imp_df['項目'] == 'NaicsSector', 'adj'] *= 0.5
+            imp_df.loc[imp_df['項目'] == 'InitialInterestRate', 'adj'] *= 0.9
             display_imp = imp_df[imp_df['項目名'] != "その他"].groupby('項目名')['adj'].sum().reset_index()
             total_adj = display_imp['adj'].sum()
             display_imp['影響度(%)'] = (display_imp['adj'] / total_adj * 100).round(1)
